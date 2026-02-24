966273b979a4d9543d9dd2804b0c3e39b9cd1196
w.alexander <will_alexander@me.com>
2026-02-24 10:38:44 +0100

    Fix AppendConditionError bug

--- src/core/event-store/append-condition.error.ts
+++ src/core/event-store/append-condition.error.ts
@@ -5,8 +5,8 @@ export class AppendConditionError extends Error {
   constructor(appendCondition: AppendCondition,
               events: DomainEvent[]) {
     super(`The following events could not be appended:
-    ${events.map(event => event.toString()).join('\n')}
-    The append condition was: ${appendCondition.toString()}.
+    ${events.map(event => JSON.stringify(event)).join('\n')}
+    The append condition was: ${JSON.stringify(appendCondition)}.
     `);
     this.name = 'AppendConditionError';
   }
